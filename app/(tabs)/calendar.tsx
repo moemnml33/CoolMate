1bd8008ae2e3f7eff5dbc8a499368637b965df5c
moe booka <mhmdbkaili@hotmail.com>
2025-03-22 00:43:06 -0400

    Default setup
    design skeleton

--- app/(tabs)/calendar.tsx
+++ app/(tabs)/calendar.tsx
@@ -3,12 +3,12 @@ import { ThemedView } from "@/components/ThemedView";
 import React from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
 
-export default function HomeScreen() {
+export default function CalendarScreen() {
   return (
     <SafeAreaView style={styles.mainContainer}>
       <ThemedView>
         <ThemedText type="title" style={styles.titleContainer}>
-          Home
+          Calendar
         </ThemedText>
       </ThemedView>
     </SafeAreaView>
